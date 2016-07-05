0be518a206f1902194e5c235ed46c5489f639dcc
Victor <hopsor@gmail.com>
2016-07-05 22:48:25 +0200

    added a dummy react component which in the future will wrap `templates/room/show.html.eex`

---
+++ web/static/js/room/conversation.js
@@ -0,0 +1,12 @@
+import React, {PropTypes} from 'react';
+// import classnames         from 'classnames';
+// import Constants          from '../../constants';
+// import { setGame }        from '../../actions/game';
+
+export default class Conversation extends React.Component {
+  render(){
+    return (
+      <p>Eventually this will wrap all the DOM included in templates/room/show.html.eex</p>
+    );
+  }
+}
