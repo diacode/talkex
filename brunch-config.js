23854cf592bb21b5ade5ffeacbd1c01bef0d912d
Victor <hopsor@gmail.com>
2016-07-05 22:21:53 +0200

    room layout

--- brunch-config.js
+++ brunch-config.js
@@ -54,6 +54,11 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/web\/static\/vendor/]
+    },
+    stylus:{
+      plugins:{
+        stylus: ['nib']
+      }
     }
   },
 
