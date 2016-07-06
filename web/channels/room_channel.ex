d129adad2f1b26bace38ba3e95486fd38375959d
Victor <hopsor@gmail.com>
2016-07-06 22:47:42 +0200

    handling new messages in `room_channel.ex`

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -14,4 +14,14 @@ defmodule Talkex.RoomChannel do
     })
     {:noreply, socket}
   end
+
+  def handle_in("new_msg", %{"body" => body}, socket) do
+    broadcast! socket, "new_msg", %{body: body, author: socket.assigns.nickname, timestamp: :os.system_time(:milli_seconds)}
+    {:noreply, socket}
+  end
+
+  def handle_out("new_msg", payload, socket) do
+    push socket, "new_msg", payload
+    {:noreply, socket}
+  end
 end
