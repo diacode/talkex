69ce8532a38c18a5e7868a245f9e3afafdaba27c
Victor <hopsor@gmail.com>
2016-07-06 21:39:26 +0200

    Fix

--- web/static/js/room/show.js
+++ web/static/js/room/show.js
@@ -4,7 +4,7 @@ import Conversation from './conversation';
 
 export default class RoomShowView {
   constructor() {
-    let conversationContainer = document.getElementById('conversationContainer');
+    let conversationContainer = document.getElementById('conversation_container');
     const dataset = conversationContainer.dataset;
     ReactDOM.render(
       <Conversation nickname={dataset.nickname} roomname={dataset.room} />,
