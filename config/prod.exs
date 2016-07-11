9e8e30ba833fa16a3db9335d63ce04545773b274
Victor <hopsor@gmail.com>
2016-07-12 01:51:51 +0200

    Added heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,17 @@ use Mix.Config
 # which you typically run after static files are built.
 config :talkex, Talkex.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "talkex.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :talkex, Talkex.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
