1bc92c5bdfd79cdc3c502508fef8d49a3b5032af
Victor <hopsor@gmail.com>
2016-07-12 01:42:30 +0200

    Added a placeholder for the video call div

--- web/static/js/room/video_call.js
+++ web/static/js/room/video_call.js
@@ -4,6 +4,10 @@ export default class VideoCall extends React.Component {
   render() {
     return (
       <div id="videocall">
+        <h2 className="not-available">
+          This space is reserved for videocalls.<br/>
+          Feature not available yet.
+        </h2>
       </div>
     );
   }
