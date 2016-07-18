3de6d5d0cb4e7f8a833b259d8e584a838fb465b2
Victor <hopsor@gmail.com>
2016-07-19 00:38:34 +0200

    changed the `host` in `prod.exs`

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :talkex, Talkex.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "talkex.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "talkex.diacode.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
